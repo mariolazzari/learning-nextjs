28c1450cf06d75c58fd1aa632d81aa961c3cb20c
Mario Lazzari <mario.lazzari@gmail.com>
2025-03-07 17:05:19 +0100

    Done chapter 7

--- blog/src/app/blog/post/[id]/page.tsx
+++ blog/src/app/blog/post/[id]/page.tsx
@@ -1,11 +1,11 @@
 import { notFound } from 'next/navigation';
 // import { posts } from '@/app/lib/placeholder-data';
 import Post from '@/app/ui/components/posts/Post';
-import { getPosts } from '@/app/lib/data';
+import { connectToDB, getPosts } from '@/app/lib/data';
 
 export default async function Page({ params }: { params: { id: string } }) {
   const posts = await getPosts();
-  const post = posts?.find((post) => post.id === ""); // empty string will never match any post (to test 404 errors)
+  const post = posts?.find((post) => post.id === params.id); // empty string will never match any post (to test 404 errors)
 
   if (!post) {
     notFound();
